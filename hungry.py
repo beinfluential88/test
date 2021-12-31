b127aa6489f318517178607c532f9888a8908e73
beinfluential88 <89182190+beinfluential88@users.noreply.github.com>
2021-12-31 10:42:10 -0800

    added donation

--- hungry.py
+++ hungry.py
@@ -3,6 +3,8 @@ hungry = input("are you hungry?")
 if hungry == "yes":
     print("Grab something to eat!")
     print("Do not grab too much thought!!")
+
+    print("Donate some food for the poor")
 else:
     print("Do your work!")
     print("You got a mail!")
